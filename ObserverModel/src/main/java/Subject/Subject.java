63c00177a51f45af1608653171eaf920ee40f0df
sciencestar <1633696870@qq.com>
2019-03-28 13:56:45 +0800

    观察者模式示例添加

---
+++ ObserverModel/src/main/java/Subject/Subject.java
@@ -0,0 +1,16 @@
+package Subject;
+
+import java.util.Observer;
+
+/***
+ * @Title: Administrator
+ * @ClassName Subject
+ * @Description: TODO 主题接口
+ * @author lxc
+ * @date 2019/3/28 13:51
+ */
+public interface Subject {
+    void registerObserver(Observer o);
+    void removeObserver(Observer o);
+    void notifyObservers();
+}
