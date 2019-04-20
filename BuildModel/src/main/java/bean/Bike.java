d1a0fd324b5db9dc98f2f63e37e5d773934c418c
sciencestar <1633696870@qq.com>
2019-04-20 20:42:22 +0800

    lombok插件使用

--- BuildModel/src/main/java/bean/Bike.java
+++ BuildModel/src/main/java/bean/Bike.java
@@ -1,11 +1,14 @@
 package bean;
 
+import lombok.Data;
+
 /**
  * @program: designModel.uml
  * @description: 自行车实体类
  * @author: liuxincheng
  * @create: 2019-03-28 09:25
  */
+@Data
 public class Bike {
 
     private String type;
@@ -13,28 +16,4 @@ public class Bike {
     private String frame;
 
     private String gps;
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getFrame() {
-        return frame;
-    }
-
-    public void setFrame(String frame) {
-        this.frame = frame;
-    }
-
-    public String getGps() {
-        return gps;
-    }
-
-    public void setGps(String gps) {
-        this.gps = gps;
-    }
 }
