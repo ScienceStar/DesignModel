bab0fe2aa5b923fa3e5e3cab976302e51abefaac
sciencestar <1633696870@qq.com>
2020-03-20 17:33:31 +0800

    update

---
+++ ProxyModel/src/main/java/client/ProxyTest.java
@@ -0,0 +1,13 @@
+package client;
+
+import inter.impl.Prox;
+
+public class ProxyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Prox proxy = new Prox();
+		proxy.Request();
+	}
+
+}
